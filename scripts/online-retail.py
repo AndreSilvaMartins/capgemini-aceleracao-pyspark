79afbe3b434d9a78a209dfd6bf90162a6644138b
Andre Martins <andre.smartins@outlook.com>
2022-04-13 15:47:02 -0300

    Resposta P6 Online Retail

--- scripts/online-retail.py
+++ scripts/online-retail.py
@@ -29,11 +29,11 @@ if __name__ == "__main__":
 	#SUBSTITUIR "," POR "." E CONVERVETER VALOR PARA FLOAT
 	df = df.withColumn('UnitPrice', F.regexp_replace(F.col('UnitPrice'),',','.').cast('float'))
 
-	#FORMATAR DATA NO FORMATO d/M/yyyy H:m E CONVERTER PARA TimestampType
-	df = (df.withColumn('InvoiceDate', F.to_timestamp(F.col('InvoiceDate'), 'd/M/yyyy H:m')))
-
 	#CRIAR CAMPO SOLD (VALOR VENDIDO Quantity*UnitPrice)
 	df = (df.withColumn('Sold', F.col("Quantity")*F.col("UnitPrice")))
+
+	#FORMATAR DATA NO FORMATO d/M/yyyy H:m E CONVERTER PARA TimestampType
+	df = (df.withColumn('InvoiceDate', F.to_timestamp(F.col('InvoiceDate'), 'd/M/yyyy H:m')))
 
 	#print(df.show())
 
@@ -41,7 +41,7 @@ if __name__ == "__main__":
 
 def p1_OR ():
 	print(spark.getOrCreate().sql(f"""
-	SELECT  ROUND(SUM(Quantity*UnitPrice),2) as Valor_Gift_Card
+	SELECT  ROUND(SUM(Sold),2) as Valor_Gift_Card
 	FROM df_online_retail
 	WHERE StockCode like 'gift_0001_%'
 	AND SUBSTRING(InvoiceNo,1,1) <> 'C'
@@ -54,7 +54,7 @@ def p2_OR ():
 	print(spark.getOrCreate().sql(f"""
 	SELECT  Year(InvoiceDate) as Year,
 			Month(InvoiceDate) as Month,
-			ROUND(SUM(Quantity*UnitPrice),2) as Valor_Gift_Card
+			ROUND(SUM(Sold),2) as Valor_Gift_Card
 	FROM df_online_retail
 	WHERE StockCode like 'gift_0001_%'
 	AND SUBSTRING(InvoiceNo,1,1) <> 'C'
@@ -68,7 +68,7 @@ def p2_OR ():
 
 def p3_OR ():
 	print(spark.getOrCreate().sql(f"""
-	SELECT  ROUND(SUM(Quantity*UnitPrice),2) as Valor_Sample
+	SELECT  ROUND(SUM(Quantity),0) as Valor_Sample
 	FROM df_online_retail
 	WHERE StockCode = 'S'
 	AND SUBSTRING(InvoiceNo,1,1) <> 'C'
@@ -94,7 +94,7 @@ def p5_OR ():
 									(SELECT YEAR(InvoiceDate) AS Ano,
 											MONTH(InvoiceDate) AS Mes,
 											Description AS Produto,
-											SUM(Sold) AS Valor_Vendas
+											ROUND(SUM(Sold),2) AS Valor_Vendas
 									FROM df_online_retail
 									WHERE SUBSTRING(InvoiceNo,1,1) <> 'C'
 									AND SUBSTRING(InvoiceNo,1,1) <> 'c'
@@ -111,4 +111,29 @@ def p5_OR ():
 									ORDER BY 1, 2
 									""").show())
 
-p5_OR()
+#p5_OR()
+
+def p6_OR ():
+	print(spark.getOrCreate().sql(f"""
+									WITH MAX_VALOR
+									AS
+									(SELECT YEAR(InvoiceDate) AS Ano,
+											MONTH(InvoiceDate) AS Mes,
+											DAY(InvoiceDate) AS Dia,
+											HOUR(InvoiceDate) AS Hora,
+											ROUND(SUM(Sold),2) AS Valor_Vendas
+									FROM df_online_retail
+									WHERE SUBSTRING(InvoiceNo,1,1) <> 'C'
+									AND SUBSTRING(InvoiceNo,1,1) <> 'c'
+									GROUP BY YEAR(InvoiceDate),
+											 MONTH(InvoiceDate), 
+											 DAY(InvoiceDate),
+											 HOUR(InvoiceDate))
+
+									SELECT  Hora,
+											Valor_Vendas as Valor_Vendido
+									FROM MAX_VALOR a
+									WHERE Valor_Vendas = (SELECT MAX(Valor_Vendas) FROM MAX_VALOR)
+									""").show())
+
+p6_OR()
